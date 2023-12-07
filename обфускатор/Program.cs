19d05f492a966e60e4c0cd8f7d266ca17bd2b84e
Цомкалов Игорь <igorcomkalov@gmail.com>
2023-12-07 08:28:16 +0300

    Добавьте файлы проекта.

---
+++ обфускатор/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "C:\\Users\\igorc\\OneDrive\\Рабочий стол\\Новый текстовый документ.txt";
+        string inputCode = File.ReadAllText(path);
+
+        // Удаление лишних комментариев
+        string codenotcoment = Regex.Replace(inputCode, @"(//.*?$)|(/\*.*?\*/)", string.Empty, RegexOptions.Multiline);
+        
+        // Замена имени класса 
+        inputCode = Regex.Replace(inputCode, @"(?<=\bclass\s+)\w+(?=\s*\{)", "New_name_of_class");
+
+        // Замена имени файла
+        string newFileName = "obfuskator.cs";
+        inputCode = Regex.Replace(inputCode, @"(?<=\bpublic\s+class\s+)\w+(?=\s*\{)", newFileName);
+
+        // Замена имени конструктора
+        inputCode = Regex.Replace(inputCode, @"public\s\w+\s*\(", "public NewConstructorName(");
+
+        // Удаление лишних пробелов и символов перехода на новую строку
+        inputCode = Regex.Replace(codenotcoment, @"\s+", " ");
+
+        // Нахождение всех идентификаторов
+        var identifiers = Regex.Matches(inputCode, @"\b\w+\b")
+                               .OfType<Match>()
+                               .Select(m => m.Value)
+                               .Distinct()
+                               .ToArray();
+
+        // Замена идентификаторов на односимвольные или двухсимвольные имена 
+        for (int i = 0; i < identifiers.Length; i++)
+        {
+            string replacement = i < 26 ? ((char)('a' + i)).ToString() : "var" + (i - 26 + 1);
+            inputCode = Regex.Replace(inputCode, $@"\b{identifiers[i]}\b", replacement);
+        }
+        Console.WriteLine("Измененный код:");
+        Console.WriteLine(inputCode);
+    }
+}
